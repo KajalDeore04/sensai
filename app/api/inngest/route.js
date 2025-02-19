2c383be2f198111f0fed1411b8c1e88096ae12e5
KajalDeore04 <kajaldeore04@gmail.com>
2025-02-19 15:34:05 +0530

    render insights and weekly

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights, helloWorld } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
@@ -8,6 +8,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
 // helloWorld,
+generateIndustryInsights
     
   ],
 });
